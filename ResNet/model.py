1c331f78a921a3f054792246c765d537b35f88df
Xingjian Leng <xingjianleng@gmail.com>
2022-11-27 01:24:30 +1100

    Add ResNet110 model training

--- ResNet/model.py
+++ ResNet/model.py
@@ -1,120 +1,94 @@
+# Based on the ResNet implementation in torchvision
+# https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
+
+import math
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from torch import nn
+from torchvision.models.resnet import conv3x3
 
 
-# Modified from TorchVision implementation of ResNet for ImageNet
 class BasicBlock(nn.Module):
-    expansion = 1
-
-    # BasicBlock in ResNet, containing two convolution layers
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        stride: int = 1,
-        downsample=None,
-    ):
-        super().__init__()
-        
-        self.conv1 = nn.Conv2d(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=3,
-            stride=stride,
-            padding=1,
-            bias=False
-        )
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(
-            in_channels=out_channels,
-            out_channels=out_channels,
-            kernel_size=3,
-            stride=1,
-            padding=1,
-            bias=False
-        )
-        self.bn2 = nn.BatchNorm2d(out_channels)
+    def __init__(self, inplanes, planes, norm_layer, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
         self.downsample = downsample
+        self.stride = stride
+        
+        self.bn1 = norm_layer(inplanes)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        
+        self.bn2 = norm_layer(planes)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
 
     def forward(self, x):
-        identity = x
+        residual = x 
+        residual = self.bn1(residual)
+        residual = self.relu1(residual)
+        residual = self.conv1(residual)
 
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
+        residual = self.bn2(residual)
+        residual = self.relu2(residual)
+        residual = self.conv2(residual)
 
-        if self.downsample:
-            identity = self.downsample(x)
-
-        out += identity
-        out = F.relu(out)
-
-        return out
+        if self.downsample is not None:
+            x = self.downsample(x)
+        return x + residual
 
 
-class ResNet(nn.Module):
-    # The ResNet implementation for CIFAR-10 dataset
-    def __init__(
-        self,
-        n: int,
-    ):
-        super().__init__()
+class Downsample(nn.Module):
+    def __init__(self, nIn, nOut, stride):
+        super(Downsample, self).__init__()
+        self.avg = nn.AvgPool2d(stride)
+        assert nOut % nIn == 0
+        self.expand_ratio = nOut // nIn
 
-        num_classes = 10  # as defined in CIFAR-10
-        self.in_channels = 16
-        self.conv1 = nn.Conv2d(3, self.in_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(self.in_channels)
+    def forward(self, x):
+        x = self.avg(x)
+        return torch.cat([x] + [x.mul(0)] * (self.expand_ratio - 1), 1)
 
-        # no subsampling in the first layer
-        self.layer1 = self._make_layer(n, 16, 1)
-        self.layer2 = self._make_layer(n, 32, 2)
-        self.layer3 = self._make_layer(n, 64, 2)
-        # output from layer3 has size (64 * 8 * 8)
-        self.avg_pool = nn.AvgPool2d(kernel_size=8)
-        # output from pooling layer has size (64 * 1 * 1)
-        self.fc = nn.Linear(64, num_classes)
 
-    def _make_layer(self, blocks, out_channels, stride):
-        # no matter how many blocks there are, this layer will half the feature size
-        # by setting stride=2, which performs subsampling
+class ResNetCifar(nn.Module):
+    def __init__(self, depth, width=1, classes=10, channels=3, norm_layer=nn.BatchNorm2d):
+        assert (depth - 2) % 6 == 0         # depth is 6N+2
+        self.N = (depth - 2) // 6
+        super(ResNetCifar, self).__init__()
+
+        # Following the Wide ResNet convention, we fix the very first convolution
+        self.conv1 = nn.Conv2d(channels, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.inplanes = 16
+        self.layer1 = self._make_layer(norm_layer, 16 * width)
+        self.layer2 = self._make_layer(norm_layer, 32 * width, stride=2)
+        self.layer3 = self._make_layer(norm_layer, 64 * width, stride=2)
+        self.bn = norm_layer(64 * width)
+        self.relu = nn.ReLU(inplace=True)
+        self.avgpool = nn.AvgPool2d(8)
+        self.fc = nn.Linear(64 * width, classes)
+
+        # Initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                
+    def _make_layer(self, norm_layer, planes, stride=1):
         downsample = None
-        layers = []
-
-        # the first block may not maintain the shape, e.g., in_channels != out_channels, stride != 1
-        # as suggested in the paper, use 1*1 convolution to transform the matrix size (option B)
-        if stride != 1 or self.in_channels != out_channels * BasicBlock.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.in_channels, out_channels * BasicBlock.expansion, kernel_size=1, stride=stride),
-                nn.BatchNorm2d(out_channels * BasicBlock.expansion),
-            )
-
-        layers.append(
-            BasicBlock(
-                in_channels=self.in_channels, out_channels=out_channels, stride=stride, downsample=downsample
-            )
-        )
-
-        self.in_channels = out_channels * BasicBlock.expansion
-        for _ in range(1, blocks):
-            # these layers maintain the shape, provided stride=1
-            layers.append(
-                BasicBlock(
-                    in_channels=self.in_channels, out_channels=out_channels
-                )
-            )
-
+        if stride != 1 or self.inplanes != planes:
+            downsample = Downsample(self.inplanes, planes, stride)
+        layers = [BasicBlock(self.inplanes, planes, norm_layer, stride, downsample)]
+        self.inplanes = planes
+        for i in range(self.N - 1):
+            layers.append(BasicBlock(self.inplanes, planes, norm_layer))
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.avg_pool(out)
-        out = torch.flatten(out, 1)
-        out = self.fc(out)
-        return out
-
-
-def ResNet110(device):
-    return ResNet(n=18).to(device=device)
+        x = self.conv1(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
