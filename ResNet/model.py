7aaf8a4c4ca0e4d66856af7cf18b3a204f82746e
Xingjian Leng <xingjianleng@gmail.com>
2022-11-29 10:02:57 +1100

    Fix the ResNet implementation

--- ResNet/model.py
+++ ResNet/model.py
@@ -1,71 +1,74 @@
-# Based on the ResNet implementation in torchvision
-# https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
-# Model implementation from
+# Based on (heavily modified) the implementation from
 # https://github.com/yueatsprograms/ttt_cifar_release/blob/acac817fb7615850d19a8f8e79930240c9afe8b5/models/ResNet.py
 
 import math
 import torch
+import torch.nn.functional as F
 from torch import nn
 from torchvision.models.resnet import conv3x3
 
 
 class BasicBlock(nn.Module):
-    def __init__(self, inplanes, planes, norm_layer, stride=1, downsample=None):
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
         self.downsample = downsample
         self.stride = stride
         
-        self.bn1 = norm_layer(inplanes)
-        self.relu1 = nn.ReLU(inplace=True)
         self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(num_features=planes)
         
-        self.bn2 = norm_layer(planes)
-        self.relu2 = nn.ReLU(inplace=True)
         self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(num_features=planes)
 
     def forward(self, x):
         residual = x 
-        residual = self.bn1(residual)
-        residual = self.relu1(residual)
+        # NOTE: In original implementation, calculation has the order BN -> ReLU -> Conv
+        #       which is clearly wrong. Changed to Conv -> BN -> ReLU as stated in original paper.
         residual = self.conv1(residual)
+        residual = self.bn1(residual)
+        residual = F.relu(residual)
 
-        residual = self.bn2(residual)
-        residual = self.relu2(residual)
         residual = self.conv2(residual)
+        residual = self.bn2(residual)
 
         if self.downsample is not None:
             x = self.downsample(x)
-        return x + residual
+        
+        # NOTE: In original implementation, ReLU was called for the residual output.
+        #       However, in ther paper ReLU was used for (x + residual)
+        return F.relu(x + residual)
 
 
 class Downsample(nn.Module):
     def __init__(self, nIn, nOut, stride):
         super(Downsample, self).__init__()
-        self.avg = nn.AvgPool2d(stride)
+        # NOTE: It was wrong in the original implementation that the
+        #       size of AvgPool kernel size equals stride. 
+        #       Rather, it should be constantly 1 as shown in FaceBook implementation
+        self.avg = nn.AvgPool2d(kernel_size=1, stride=stride)
         assert nOut % nIn == 0
         self.expand_ratio = nOut // nIn
 
     def forward(self, x):
         x = self.avg(x)
-        return torch.cat([x] + [x.mul(0)] * (self.expand_ratio - 1), 1)
+        # In original FaceBook Lua implementation, dim=2 because index starts with 1 in Lua
+        return torch.cat([x] + [torch.zeros_like(x)] * (self.expand_ratio - 1), dim=1)
 
 
 class ResNetCifar(nn.Module):
-    def __init__(self, depth, width=1, classes=10, channels=3, norm_layer=nn.BatchNorm2d):
+    def __init__(self, depth, classes=10, channels=3):
         assert (depth - 2) % 6 == 0         # depth is 6N+2
         self.N = (depth - 2) // 6
         super(ResNetCifar, self).__init__()
 
-        # Following the Wide ResNet convention, we fix the very first convolution
         self.conv1 = nn.Conv2d(channels, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn = nn.BatchNorm2d(num_features=16)
         self.inplanes = 16
-        self.layer1 = self._make_layer(norm_layer, 16 * width)
-        self.layer2 = self._make_layer(norm_layer, 32 * width, stride=2)
-        self.layer3 = self._make_layer(norm_layer, 64 * width, stride=2)
-        self.bn = norm_layer(64 * width)
-        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(16, stride=1)
+        self.layer2 = self._make_layer(32, stride=2)
+        self.layer3 = self._make_layer(64, stride=2)
         self.avgpool = nn.AvgPool2d(8)
-        self.fc = nn.Linear(64 * width, classes)
+        self.fc = nn.Linear(64, classes)
 
         # Initialization
         for m in self.modules():
@@ -73,23 +76,24 @@ class ResNetCifar(nn.Module):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
                 
-    def _make_layer(self, norm_layer, planes, stride=1):
+    def _make_layer(self, planes, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes:
             downsample = Downsample(self.inplanes, planes, stride)
-        layers = [BasicBlock(self.inplanes, planes, norm_layer, stride, downsample)]
+        layers = [BasicBlock(self.inplanes, planes, stride, downsample)]
         self.inplanes = planes
-        for i in range(self.N - 1):
-            layers.append(BasicBlock(self.inplanes, planes, norm_layer))
+        for _ in range(self.N - 1):
+            layers.append(BasicBlock(self.inplanes, planes))
         return nn.Sequential(*layers)
 
     def forward(self, x):
+        # NOTE: It was wrong in the implementation that BatchNorm and ReLU were put afte all layers
         x = self.conv1(x)
+        x = self.bn(x)
+        x = F.relu(x)
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
-        x = self.bn(x)
-        x = self.relu(x)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
--- ResNet/train.py
+++ ResNet/train.py
@@ -7,10 +7,67 @@ sys.path.append(".")
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.optim.lr_scheduler import StepLR
-from torchvision import transforms, datasets
+from torch.optim.lr_scheduler import MultiStepLR
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+import numpy as np
+from sklearn.model_selection import train_test_split
 
+import copy
+import pickle
 from model import ResNetCifar
+
+
+class CIFARDataset(Dataset):
+    def __init__(self, images, labels, transform):
+        self.images = images
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.labels)
+    
+    def __getitem__(self, idx):
+        image = self.images[idx]
+        label = self.labels[idx]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+
+# function used to extract 
+def unpickle(path):
+    with open(path, 'rb') as fo:
+        data = pickle.load(fo, encoding='bytes')
+    return data
+
+
+def load_data(root):
+    # image data is stored as N * W * H * C
+    # 50000 training data, 10000 testing data
+    train_x, train_y = np.zeros((50000, 32, 32, 3), dtype=np.uint8), []
+    test_y = []
+
+    # extract training data
+    for i in range(1, 6):
+        data_path = f"{root}/data_batch_{i}"
+        data = unpickle(path=data_path)
+        train_x[(i - 1) * 10000: i * 10000] = data[b'data'].reshape(10000, 3, 32, 32).transpose((0, 2, 3, 1))
+        train_y.extend(data[b'labels'])
+
+    # extract testing data
+    data_path = f"{root}/test_batch"
+    data = unpickle(path=data_path)
+    test_x = data[b'data'].reshape(10000, 3, 32, 32).transpose((0, 2, 3, 1))
+    test_y = data[b'labels']
+
+    # convert to numpy arrays
+    train_y = np.array(train_y)
+    test_y = np.array(test_y)
+
+    # split the validation set (45000 train, 5000 val)
+    train_x, val_x, train_y, val_y = train_test_split(train_x, train_y, test_size=0.1)
+    return train_x, val_x, test_x, train_y, val_y, test_y
 
 
 def train(args, model, device, train_loader, optimizer, epoch):
@@ -45,21 +102,24 @@ def test(model, device, test_loader):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+    
+    # return the validation/test accuracy
+    return correct / len(test_loader.dataset)
 
 
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch ResNet Train')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
+    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                        help='input batch size for training (default: 128)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=40, metavar='N',
-                        help='number of epochs to train (default: 40)')
+    parser.add_argument('--epochs', type=int, default=180, metavar='N',
+                        help='number of epochs to train (default: 180)')
     parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
                         help='learning rate (default: 0.1)')
-    parser.add_argument('--gamma', type=float, default=0.9, metavar='M',
-                        help='Learning rate step gamma (default: 0.9)')
+    parser.add_argument('--gamma', type=float, default=0.1, metavar='M',
+                        help='Learning rate step gamma (default: 0.1)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='disables CUDA training')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
@@ -88,24 +148,54 @@ def main():
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
-    train_loader = torch.utils.data.DataLoader(
-        datasets.CIFAR10("../data", train=True, transform=transform_train, download=True),
-        batch_size=args.batch_size, shuffle=True, **kwargs)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.CIFAR10("../data", train=False, transform=transform_test, download=True),
-        batch_size=args.test_batch_size, shuffle=False, **kwargs)
+    # extract data and load to dataset
+    train_x, val_x, test_x, train_y, val_y, test_y = load_data("../data/cifar-10-batches-py")
+    train_set = CIFARDataset(train_x, train_y, transform_train)
+    val_set = CIFARDataset(val_x, val_y, transform_test)
+    test_set = CIFARDataset(test_x, test_y, transform_test)
+
+    # put dataset to dataloader
+    train_loader = DataLoader(
+        dataset=train_set,
+        batch_size=args.batch_size,
+        shuffle=True,
+        **kwargs,
+    )
+    val_loader = DataLoader(
+        dataset=val_set,
+        batch_size=args.test_batch_size,
+        shuffle=False,
+        **kwargs,
+    )
+    test_loader = DataLoader(
+        dataset=test_set,
+        batch_size=args.test_batch_size,
+        shuffle=False,
+        **kwargs,
+    )
 
     model = ResNetCifar(depth=110).to(device)
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-4)
+    scheduler = MultiStepLR(optimizer, milestones=[90, 135], gamma=args.gamma)
 
+    best_val_acc = 0.0
+    best_model = None
     for epoch in range(1, args.epochs + 1):
-        print(f"Epoch {epoch} with learning rate {optimizer.param_groups[0]['lr']}")
+        print(f"Epoch {epoch} with learning rate: {optimizer.param_groups[0]['lr']}")
         train(args, model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader)
+        new_val_acc = test(model, device, val_loader)
+        # store the best model on the validation set
+        if new_val_acc > best_val_acc:
+            best_model = copy.deepcopy(model.state_dict())
+            best_val_acc = new_val_acc
         scheduler.step()
+    
+    # load the best state_dict and test on test setc
+    model.load_state_dict(best_model)
+    test(model, device, test_loader)
+
     if args.save_model:
-        torch.save(model.state_dict(), f"../model/resnet110-{args.epochs}.pt")
+        torch.save(best_model, f"../model/resnet110-{args.epochs}.pt")
 
 
 if __name__ == '__main__':
